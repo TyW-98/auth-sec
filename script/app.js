5e0d018564a057eb788d820577437b6ee64c3d48
wey1998 <yitwey1998@hotmail.com>
2023-04-27 23:07:26 +0100

    Added content to register.ejs to create register page

--- script/app.js
+++ script/app.js
@@ -21,13 +21,19 @@ app
     res.redirect("/" + req.body.submitBtn);
   });
 
-app.route("/register").get((req, res) => {
-  res.render("register");
-});
+app
+  .route("/register")
+  .get((req, res) => {
+    res.render("register");
+  })
+  .post((req, res) => {
+    console.log(req.body.email);
+    console.log(req.body.password);
+  });
 
 app.route("/login").get((req, res) => {
-    res.render('login');
-})
+  res.render("login");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
