ed55a4b6eb7837a7772a5ee2aaa5b22ba65a0530
wey1998 <yitwey1998@hotmail.com>
2023-05-11 23:20:19 +0100

    Add level one security

--- script/app.js
+++ script/app.js
@@ -19,29 +19,31 @@ const userSchema = new mongoose.Schema({
   },
   username: {
     type: String,
-    required: [true], 
+    required: [true],
   },
   password: {
     type: String,
-    required: [true], 
-  }
-})
+    required: [true],
+  },
+});
 
 const User = new mongoose.model("User", userSchema);
 
-const adminUser = new User ({
-  email: 'admin@example.com',
-  username: 'admin', 
-  password: 'admin'
-})
+const adminUser = new User({
+  email: "admin@example.com",
+  username: "admin",
+  password: "admin",
+});
 
-User.find().then((users) => {
-  if (users.length === 0) {
-    User.insertMany(adminUser)
-  }
-}).catch((err) => {
-  console.log(err);
-})
+User.find()
+  .then((users) => {
+    if (users.length === 0) {
+      User.insertMany(adminUser);
+    }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app
   .route("/")
@@ -58,13 +60,13 @@ app
     res.render("register");
   })
   .post((req, res) => {
-    const newUser = new User ({
+    const newUser = new User({
       email: req.body.email,
       username: req.body.username,
-      password: req.body.password
-    })
-    newUser.save()
-
+      password: req.body.password,
+    });
+    newUser.save();
+    res.render("post");
   });
 
 app
@@ -73,8 +75,18 @@ app
     res.render("login");
   })
   .post((req, res) => {
-    console.log(req.body.email);
-    console.log(req.body.password);
+    User.findOne({ username: req.body.username })
+      .then((user) => {
+        if (user) {
+          if (user.password === req.body.password) {
+            res.render("post");
+          } else {
+          }
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 
 app
@@ -92,18 +104,18 @@ app.route("/recipe/:recipeID").get((req, res) => {
   res.render("recipe");
 });
 
-app.route("/post").get((req, res) => {
-  res.render("post");
-}).post((req, res) => {
-  console.log(req.body);
-})
+// app.route("/post").get((req, res) => {
+//   res.render("post");
+// }).post((req, res) => {
+//   console.log(req.body);
+// })
 
 app.route("/ingredients").post((req, res) => {
-  const {inregredients} = req.body;
+  const { inregredients } = req.body;
   console.log(inregredients);
 
   res.sendStatus(200);
-})
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
